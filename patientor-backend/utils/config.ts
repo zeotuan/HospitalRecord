89f24b73169682b1058ab1823cee3afe32880885
zeotuan <zeotuan@gmail.com>
2021-05-12 17:22:53 +1000

    update config to only return MONGO_URI as string

--- patientor-backend/utils/config.ts
+++ patientor-backend/utils/config.ts
@@ -2,10 +2,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 const PORT = process.env.PORT || 3001;
 
-let MONGODB_URI = process.env.MONGODB_URI;
+let GET_MONGODB_URI = process.env.MONGODB_URI;
 if(process.env.NODE_ENV === 'test'){
-    MONGODB_URI = process.env.TEST_MONGODB_URI;
+    GET_MONGODB_URI = process.env.TEST_MONGODB_URI;
+}else if(process.env.NODE_ENV === 'dev'){
+    GET_MONGODB_URI = process.env.DEV_MONGODB_URI;
 }
+const MONGODB_URI:string = GET_MONGODB_URI? GET_MONGODB_URI : '';
 
 const JWT_SECRET = process.env.JWT_SECRET;
 const saltRound = process.env.saltRound;
@@ -17,7 +20,7 @@ const config = {
     JWT_SECRET,
     saltRound,
     passwordRegex,
-    usernameRegex
+    usernameRegex,
 };
 
 export default config;
