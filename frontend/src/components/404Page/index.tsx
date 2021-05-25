3b6a680778dcfdfaa7d0b0c1c56c82a62239f838
zeotuan <zeotuan@gmail.com>
2021-05-25 14:57:34 +1000

    fix bug : cannot resolve module *.jpq

--- frontend/src/components/404Page/index.tsx
+++ frontend/src/components/404Page/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container } from 'semantic-ui-react';
-import pic from '../../images/doge404.jpq';
+import pic from '../../images/doge404.jpg';
 
 const NotFoundPage = () => {
     return(
